70373de728786785c306c66c582dc39a73fef1d8
Josh Beridon <jberidon@gmail.com>
2016-04-16 23:34:00 -0500

    Can now zoom in and out

--- app/src/main/java/iso/io/iso/CameraActivity.java
+++ app/src/main/java/iso/io/iso/CameraActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -23,6 +24,7 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -37,6 +39,7 @@ public class CameraActivity extends AppCompatActivity {
   TextView diagram;
   PictureMesher pictureMesher;
   Boolean picturesFinished;
+  float distance;
   private final Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
     @Override public void onShutter() {
       Log.e(TAG, "onShutter");
@@ -57,31 +60,31 @@ public class CameraActivity extends AppCompatActivity {
       Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
       bitmapMap.put(currentSide, bitmap);
       doneTakingPicture();
-
     }
   };
 
   private void doneTakingPicture() {
     // check if weve gotten all the pictures
-    if(currentSide.equals(PictureMesher.PictureSide.FRONT)){
-      pictureMesher.addPicture(bitmapMap.get(currentSide),currentSide);
+    if (currentSide.equals(PictureMesher.PictureSide.FRONT)) {
+      pictureMesher.addPicture(bitmapMap.get(currentSide), currentSide);
       currentSide = PictureMesher.PictureSide.TOP;
-    } else if(currentSide.equals(PictureMesher.PictureSide.TOP)){
-      pictureMesher.addPicture(bitmapMap.get(currentSide),currentSide);
+    } else if (currentSide.equals(PictureMesher.PictureSide.TOP)) {
+      pictureMesher.addPicture(bitmapMap.get(currentSide), currentSide);
       currentSide = PictureMesher.PictureSide.RIGHT;
     } else {
-        picturesFinished = true;
-        pictureMesher.doCalcs(new MeshWorkerCallback() {
-          @Override public void meshWorkerCompleted(Object data) {
-            //TODO DONE
-            Toast.makeText(CameraActivity.this, "DO CALCS CALBACK BITCHES", Toast.LENGTH_SHORT).show();
-          }
-        });
+      picturesFinished = true;
+      pictureMesher.doCalcs(new MeshWorkerCallback() {
+        @Override public void meshWorkerCompleted(Object data) {
+          //TODO DONE
+          Toast.makeText(CameraActivity.this, "DO CALCS CALBACK BITCHES", Toast.LENGTH_SHORT)
+              .show();
+        }
+      });
     }
     // if not
     this.runOnUiThread(new Runnable() {
       @Override public void run() {
-        if(picturesFinished){
+        if (picturesFinished) {
           diagram.setVisibility(View.INVISIBLE);
         }
         diagram.setText(currentSide.getAsString());
@@ -105,6 +108,7 @@ public class CameraActivity extends AppCompatActivity {
     diagram.setText(currentSide.getAsString());
     pictureMesher = new PictureMesher();
     picturesFinished = false;
+    FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
 
     requestCamera();
     disableFinished();
@@ -124,8 +128,6 @@ public class CameraActivity extends AppCompatActivity {
       }
     });
   }
-
-
 
   private static File getOutputMediaFile() {
     File mediaStorageDir =
@@ -209,6 +211,71 @@ public class CameraActivity extends AppCompatActivity {
 
   @Override protected void onResume() {
     super.onResume();
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    // Get the pointer ID
+    Camera.Parameters params = camera.getParameters();
+    int action = event.getAction();
+
+
+    if (event.getPointerCount() > 1) {
+      // handle multi-touch events
+      if (action == MotionEvent.ACTION_POINTER_DOWN) {
+        distance = getFingerDistance(event);
+      } else if (action == MotionEvent.ACTION_MOVE && params.isZoomSupported()) {
+        camera.cancelAutoFocus();
+        handleZoom(event, params);
+      }
+    } else {
+      // handle single touch events
+      if (action == MotionEvent.ACTION_UP) {
+        handleFocus(event, params);
+      }
+    }
+    return true;
+  }
+  private void handleZoom(MotionEvent event, Camera.Parameters params) {
+    int maxZoom = params.getMaxZoom();
+    int zoom = params.getZoom();
+    float newDistance = getFingerDistance(event);
+    if (newDistance > this.distance) {
+      //zoom in
+      if (zoom < maxZoom)
+        zoom++;
+    } else if (newDistance < distance) {
+      //zoom out
+      if (zoom > 0)
+        zoom--;
+    }
+    distance = newDistance;
+    params.setZoom(zoom);
+    camera.setParameters(params);
+  }
+
+  public void handleFocus(MotionEvent event, Camera.Parameters params) {
+    int pointerId = event.getPointerId(0);
+    int pointerIndex = event.findPointerIndex(pointerId);
+    // Get the pointer's current position
+    float x = event.getX(pointerIndex);
+    float y = event.getY(pointerIndex);
+
+    List<String> supportedFocusModes = params.getSupportedFocusModes();
+    if (supportedFocusModes != null && supportedFocusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+      camera.autoFocus(new Camera.AutoFocusCallback() {
+        @Override
+        public void onAutoFocus(boolean b, Camera camera) {
+          // currently set to auto-focus on single touch
+        }
+      });
+    }
+  }
+
+  private float getFingerDistance(MotionEvent event) {
+    float x = event.getX(0) - event.getX(1);
+    float y = event.getY(0) - event.getY(1);
+    return (float) Math.sqrt(x * x + y * y);
   }
 }
 
